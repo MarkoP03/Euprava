02fc9eec76544171b3512dbb2556d0cf802822da
BranislavaSabovljev <sabovljev.branislava@gmail.com>
2026-01-27 00:33:27 +0100

    upate

--- Euprava/kindergarten/src/main/java/com/example/Euprava/model/User.java
+++ Euprava/kindergarten/src/main/java/com/example/Euprava/model/User.java
@@ -63,7 +63,7 @@ public class User implements UserDetails {
     @Override
     public boolean isAccountNonLocked() {
         return true;
-    }
+    } 
 
     @Override
     public boolean isCredentialsNonExpired() {
